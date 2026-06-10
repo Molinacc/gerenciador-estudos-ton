f96eac5249dea08de06d0ac3b6a172398d521ae1
Molinacc <molinacc@hotmail.com>
2026-06-10 09:37:57 -0300

    Add app/src/main/java/br/com/ton/estudos/presentation/theme/Color.kt

---
+++ app/src/main/java/br/com/ton/estudos/presentation/theme/Color.kt
@@ -0,0 +1,67 @@
+package br.com.ton.estudos.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary palette - Deep Indigo
+val Indigo10 = Color(0xFF0D0A2E)
+val Indigo20 = Color(0xFF1A1456)
+val Indigo30 = Color(0xFF271E7E)
+val Indigo40 = Color(0xFF3428A6)
+val Indigo50 = Color(0xFF4032CE)
+val Indigo60 = Color(0xFF5C6BC0)
+val Indigo70 = Color(0xFF7986CB)
+val Indigo80 = Color(0xFF9FA8DA)
+val Indigo90 = Color(0xFFC5CAE9)
+val Indigo95 = Color(0xFFE8EAF6)
+val Indigo99 = Color(0xFFF5F5FF)
+
+// Secondary palette - Teal
+val Teal20 = Color(0xFF003733)
+val Teal30 = Color(0xFF005048)
+val Teal40 = Color(0xFF006B5E)
+val Teal50 = Color(0xFF008777)
+val Teal60 = Color(0xFF26A69A)
+val Teal70 = Color(0xFF4DB6AC)
+val Teal80 = Color(0xFF80CBC4)
+val Teal90 = Color(0xFFB2DFDB)
+val Teal99 = Color(0xFFE0F2F1)
+
+// Tertiary palette - Amber
+val Amber30 = Color(0xFF4E2B00)
+val Amber40 = Color(0xFF6E3D00)
+val Amber50 = Color(0xFF965200)
+val Amber60 = Color(0xFFC26B00)
+val Amber70 = Color(0xFFF59500)
+val Amber80 = Color(0xFFFFB74D)
+val Amber90 = Color(0xFFFFCC80)
+val Amber99 = Color(0xFFFFF8E1)
+
+// Error
+val Red10 = Color(0xFF410002)
+val Red20 = Color(0xFF690005)
+val Red30 = Color(0xFF93000A)
+val Red40 = Color(0xFFBA1A1A)
+val Red80 = Color(0xFFFFB4AB)
+val Red90 = Color(0xFFFFDAD6)
+
+// Neutral
+val Neutral10 = Color(0xFF1A1A2E)
+val Neutral17 = Color(0xFF232336)
+val Neutral22 = Color(0xFF2D2D42)
+val Neutral24 = Color(0xFF303045)
+val Neutral87 = Color(0xFFDEDEED)
+val Neutral92 = Color(0xFFEAEAF5)
+val Neutral94 = Color(0xFFEEEEF7)
+val Neutral96 = Color(0xFFF2F2FA)
+val NeutralVariant30 = Color(0xFF3D3D55)
+val NeutralVariant50 = Color(0xFF666680)
+val NeutralVariant60 = Color(0xFF808099)
+val NeutralVariant80 = Color(0xFFB5B5CC)
+val NeutralVariant90 = Color(0xFFD4D4E8)
+
+// Subject colors
+val SubjectColors = listOf(
+    "#5C6BC0", "#26A69A", "#EF5350", "#FFA726",
+    "#66BB6A", "#AB47BC", "#29B6F6", "#FF7043",
+    "#8D6E63", "#78909C", "#EC407A", "#26C6DA"
+)
